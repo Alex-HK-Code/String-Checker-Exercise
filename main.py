b375ae30c8c929e12c399546aedbe2094ce47306
Alex Fung <alexfungshunhang@gmail.com>
2020-12-28 17:21:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ def beginWithVowel(s):
         return False 
     if(s[0] == 'a' or s[0] == 'e' or s[0] == 'i' or s[0] == 'o' or s[0] == 'u'):
         return True 
-    return False 
+    return False #return false if the above condition is not fulfilled
 
 
 while(True):
-    user_string = input("Enter 'Exit' to exit the program | Enter a string that you want to check:\n")
+    user_string = input("Enter 'exit' to exit the program | Enter a string that you want to check:\n")
 
-    if(user_string == "exit"):
+    if(user_string == "exit"):  #break the while loop
         break
 
-    if(beginWithVowel(user_string)):
+    if(beginWithVowel(user_string)):   #if beginWithVowel returns true, enter the block 
         print("It begins with a vowel")
 
     
